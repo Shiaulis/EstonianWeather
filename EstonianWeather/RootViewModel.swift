972146e41e5429d7cd99c02ea2a9bc47b36de589
Andrius Shiaulis <shiaulis@gmail.com>
2020-01-14 19:13:14 +0200

    Reorganize project structure

---
+++ EstonianWeather/RootViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  RootViewModel.swift
+//  EstonianWeather
+//
+//  Created by Andrius Shiaulis on 13.01.2020.
+//  Copyright © 2020 Andrius Shiaulis. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+final class RootViewMolel {
+
+    private var disposables: Set<AnyCancellable> = []
+    private(set) var data: Data?
+
+    var networkPublisher: AnyPublisher<(data: Data, response: URLResponse), URLError> {
+        let url = URL(string: "http://www.ilmateenistus.ee/ilma_andmed/xml/forecast.php?lang=eng")!
+        let session  = URLSession.shared
+        return session.dataTaskPublisher(for: url)
+        .eraseToAnyPublisher()
+    }
+
+    func fetch(completion: @escaping (Result<Data, Error>) -> Void) {
+        networkPublisher.sink(receiveCompletion: {
+            print("received completion", $0)
+        }) {
+            completion(.success($0.data))
+        }
+        .store(in: &disposables)
+    }
+
+    func map() {
+        self.networkPublisher
+            .map { WeatherParser().parse(data: $0.data, serviceInfo: .init(date: Date(), languageCode: "en")) }
+            .sink(receiveCompletion: <#T##((Subscribers.Completion<URLError>) -> Void)##((Subscribers.Completion<URLError>) -> Void)##(Subscribers.Completion<URLError>) -> Void#>, receiveValue: { (<#Result<EWDocument, WeatherParser.Error>#>) in
+                <#code#>
+            })
+    }
+
+}
