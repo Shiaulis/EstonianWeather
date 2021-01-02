6f2143dadf12664e94988e9021ed758f44ec3475
Andrius Shiaulis <shiaulis@gmail.com>
2021-01-02 14:04:56 +0200

    Add fastlane to project

---
+++ fastlane/Appfile.swift
@@ -0,0 +1,9 @@
+var appIdentifier: String { return "com.shiaulis.EstonianWeather" } // The bundle identifier of your app
+var appleID: String { return "shiaulis@gmail.com" } // Your Apple email address
+
+var itcTeam: String? { return "122193192" } // App Store Connect Team ID
+var teamID: String { return "NFA8F38A32" } // Apple Developer Portal Team ID
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
