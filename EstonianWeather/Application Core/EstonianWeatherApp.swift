190bb6ef0231839a1b9424e999ccdc38f3463741
Andrius Shiaulis <shiaulis@gmail.com>
2021-01-03 11:46:43 +0200

    Remove redundant code from main file

--- EstonianWeather/Application Core/EstonianWeatherApp.swift
+++ EstonianWeather/Application Core/EstonianWeatherApp.swift
@@ -15,33 +15,18 @@ enum ApplicationMode {
 struct EstonianWeatherApp: App {
 
     private let applicationController = ApplicationController()
-    private func clearLaunchScreenCache() {
-        do {
-            try FileManager.default.removeItem(atPath: NSHomeDirectory()+"/Library/SplashBoard")
-        } catch {
-            print("Failed to delete launch screen cache: \(error)")
-        }
-    }
-
-    private func delayLaunch() {
-        try? FileManager.default.removeItem(atPath: NSHomeDirectory()+"/Library/SplashBoard")
-
-        do {
-            sleep(2)
-        }
-    }
 
     var body: some Scene {
         WindowGroup {
             if self.applicationController.applicationMode != .unitTests {
                 TabbarView(
-                    forecastListView: ForecastListView(viewModel: ForecastListViewModel(model:self.applicationController.model)),
-                    settingsView: SettingsView(viewModel: SettingsViewModel(ratingService: self.applicationController.ratingService))
+                    forecastListView: ForecastListView(
+                        viewModel: ForecastListViewModel(model:self.applicationController.model)
+                    ),
+                    settingsView: SettingsView(
+                        viewModel: SettingsViewModel(ratingService: self.applicationController.ratingService)
+                    )
                 )
-                .onAppear {
-                    clearLaunchScreenCache()
-                    delayLaunch()
-                }
             }
             else {
                 AnyView(Text(R.string.localizable.unitTestingMode()))
