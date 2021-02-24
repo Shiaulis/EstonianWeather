76507491fde7b1af6eb992c5794da6eac0c17fe2
Andrius Shiaulis <shiaulis@gmail.com>
2021-02-24 15:01:35 +0200

    In-app purchase prototype

--- EstonianWeather/Views/Settings/SettingsView.swift
+++ EstonianWeather/Views/Settings/SettingsView.swift
@@ -7,10 +7,13 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct SettingsView: View {
 
     let viewModel: SettingsViewModel
+    @State private var purchased = false
+    @ObservedObject private var products = ProductsDB.shared
 
     var body: some View {
         NavigationView {
@@ -26,6 +29,16 @@ struct SettingsView: View {
                     NavigationLink(destination: AboutMeView()) {
                         Text(R.string.localizable.aboutMeTitle())
                     }
+                    List {
+                        ForEach((0 ..< self.products.items.count), id: \.self) { column in
+                            let product = self.products.items[column]
+
+                            Text(description(for: product))
+                                .onTapGesture {
+                                    _ = IAPManager.shared.purchase(product: self.products.items[column])
+                                }
+                        }
+                    }
                 }
 
                 Section {
@@ -38,6 +51,9 @@ struct SettingsView: View {
             }
             .navigationBarTitle(R.string.localizable.settings())
             .navigationBarColor(backgroundColor: .appRose, tintColor: .white)
+            .onAppear {
+                IAPManager.shared.getProducts()
+            }
             .onDisappear(perform: {
                             if let scene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
                                 self.viewModel.makeAttemptToShowRating(in: scene)
@@ -48,10 +64,26 @@ struct SettingsView: View {
         .navigationViewStyle(StackNavigationViewStyle())
     }
 
+    private func description(for product: SKProduct) -> String {
+        let title = product.localizedTitle
+        let priceTitle = product.localizedPrice
+
+        return "\(title) (\(priceTitle))"
+    }
+
 }
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsView(viewModel: SettingsViewModel(ratingService: AppStoreRatingService()))
+    }
+}
+
+extension SKProduct {
+    var localizedPrice: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = priceLocale
+        return formatter.string(from: price)!
     }
 }
